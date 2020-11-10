7ca7efdca48998a9aa25e4bdbb5ff31d61e7f17e
Karim Ratib <karim.ratib@gmail.com>
2020-11-09 20:25:12 -0800

    XPath module for testing

--- test/musicxml.spec.js
+++ test/musicxml.spec.js
@@ -2,6 +2,8 @@ import assert from 'assert';
 import fs from 'fs';
 import regeneratorRuntime from 'regenerator-runtime';
 import {validateXMLWithXSD} from 'validate-with-xmllint';
+import select from 'xpath.js';
+import {DOMParser} from 'xmldom';
 import {Playlist, Song} from '../src/parser';
 import {MusicXML} from '../src/musicxml';
 
@@ -22,6 +24,13 @@ describe('MusicXML', function() {
       failed = true;
     }
     assert.strictEqual(failed, true, 'Expected XML validator to fail for invalid.xml');
+  });
+
+  it('should extract information from MusicXML files', function () {
+    const doc = new DOMParser().parseFromString(fs.readFileSync('test/data/bolivia.xml', 'utf-8'));
+    const composer = select(doc, '//creator[@type="composer"]/text()')
+    assert.strictEqual(composer.length, 1);
+    assert.strictEqual(composer[0].toString(), "Cedar Walton");
   });
 
   it('should create a valid MusicXML for an iReal song', async function() {
