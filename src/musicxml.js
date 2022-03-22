62824865f540f3bb36729e7a5728f29ebd00f0dc
infojunkie <karim.ratib@gmail.com>
2022-03-21 22:09:45 -0700

    Generate cue notes #33

--- src/musicxml.js
+++ src/musicxml.js
@@ -29,6 +29,7 @@ export class MusicXML {
   static sequenceNote = [
     // Expected order of note elements.
     // https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/note/
+    'cue',
     'pitch',
     'rest',
     'unpitched',
@@ -884,13 +885,17 @@ export class MusicXML {
     }
 
     return MusicXML.reorderSequence(this.measure, [noteType, {
+      _name: 'cue'
+    }, {
       'notehead': this.options.notehead
     }, {
       'duration': duration.duration
     }, {
       'voice': 1,
     }, {
-      'type': duration.type
+      _name: 'type',
+      _attrs: { 'size': 'full' },
+      _content: duration.type
     }, { ...(tie && {
       _name: 'tie',
       _attrs: { 'type': tie }
